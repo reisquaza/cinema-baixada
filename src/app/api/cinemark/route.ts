b117f199ea617d11b27ec180462039604d6effed
João Victor Reis <joaovictor.reis2000@gmail.com>
2025-01-09 19:05:55 +0000

    fix: cinemark test api

--- src/app/api/cinemark/route.ts
+++ src/app/api/cinemark/route.ts
@@ -1,9 +1,10 @@
 import { CinemarkServiceFactory } from "@/factories/CinemarkServiceFactory";
-import { MovieServiceFactory } from "@/factories/MovieServiceFactory";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    const movies = await CinemarkServiceFactory.getFormatedMovies();
+    const movies = await CinemarkServiceFactory.getFormatedMovies(
+        "677e9e875e5b1486fb0ed2de"
+    );
     for (let index = 0; index < movies.length; index++) {
         // await MovieServiceFactory.create(movies[index]);
     }
